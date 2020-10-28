2959f52b25cc7117ad30a45eb472adfbe3fbc8fd
Alejandro-Ochando <alejandro.ochando@live.com>
2020-10-28 19:09:41 +0100

    Creando el formulario, validación y Mensaje de Error

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Formulario from './components/Formulario';
 
 function App() {
+
+  const [ search, saveSearch ] = useState('');
+
   return (
-    <div><h1>Buscador de Imagenes</h1></div>
-  );
+    <div className="container">
+      <div className="jumbotron">
+        <p className="lead text-center" >Buscador de Imagenes</p>
+        <Formulario 
+          saveSearch={saveSearch}
+        />
+      </div>
+    </div>
+  ); 
 }
 
 export default App;
---
+++ src/components/Formulario.js
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import Error from './Error';
+
+const Formulario = ({ saveSearch }) => {
+    
+    const [ term, saveTerm ] = useState('');
+    const [ error, saveError ] = useState(false);
+    
+    const handlerSubmit = e => {
+        e.preventDefault();
+        
+        //Validación
+        if( term.trim() === '') {
+            saveError(true);
+            return;
+        }
+        saveError(false);
+        saveSearch(term);
+    }
+
+    return ( 
+        <form
+            onSubmit={handlerSubmit}
+        >
+            <div className="row">
+                <div className="form-group col-md-8">
+                    <input 
+                        type="text"
+                        className="form-control form-control-lg"
+                        placeholder="Busca una imagen, ej: fútbol"
+                        onChange={ e => saveTerm(e.target.value)}
+                    />
+                </div>   
+                <div className="form-group col-md-4">
+                    <input 
+                        type="submit"
+                        className="btn btn-lg btn-danger btn-block"
+                        vale="Buscar"
+                    />
+                </div>   
+            </div>
+            {error ? <Error mensaje="Agrega un término de búsqueda" /> : null }
+        </form>
+     );
+}
+ 
+export default Formulario;
