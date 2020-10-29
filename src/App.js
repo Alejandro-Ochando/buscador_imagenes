a1bac17c018d1125aa8814ec995e0736a764e5bf
Alejandro-Ochando <alejandro.ochando@live.com>
2020-10-29 17:34:13 +0100

    Consultando la API y obteniendo el Array de fotos

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-import React, { useState } from 'react';
+import Axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import Formulario from './components/Formulario';
 
 function App() {
 
   const [ search, saveSearch ] = useState('');
+
+  useEffect(() => {
+    
+    const consultAPI = async() => {
+
+    if(search === '') return;
+    
+    const imagesPerPage = 30;
+    const key ='18907144-ac5d4537a4499596029ff3be0';
+    const url=`https://pixabay.com/api/?key=${key}&q=${search}&per_page=${imagesPerPage}`;
+    
+    const answer = await axios.get(url);
+
+    saveSearch(answer.data.hits);
+    }
+    consultAPI();
+  }, [search]);
+
 
   return (
     <div className="container">
