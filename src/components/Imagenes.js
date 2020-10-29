5789ac224964e004ca0d13ed6e98d77c3b790b9d
Alejandro-Ochando <alejandro.ochando@live.com>
2020-10-29 18:50:57 +0100

    Mostrando las imagenes y enlaces HD

--- src/components/Imagenes.js
+++ src/components/Imagenes.js
@@ -1,14 +1,27 @@
 import React from 'react';
+import './Imagenes.css';
 
 
 const Imagenes = ({ image }) => {
-    const {largeImageURL, likes, previewURL, tags, view } = image;
+    const {largeImageURL, likes, previewURL, tags, views } = image;
     
 
     return ( 
         <div className="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
             <div className="card">
-                <img src={previewURL} alt={tags} className="card-img-top" />
+                <img src={previewURL} alt={tags} className="card-img-top"  />
+                <div className="card-body">
+                    <p className="card-text">{likes} Likes</p>
+                    <p className="card-text">{views} Visitas</p>
+                </div>
+                <div className="card-footer">
+                    <a
+                        href={largeImageURL}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="btn btn-primary btn-block"
+                    >Ver Imagen</a>
+                </div>
             </div>
         </div>
      );
